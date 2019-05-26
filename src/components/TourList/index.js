841def9b38a7b6779d4b8c339b00fc73a561b09f
Zhen Guang <null>
2019-05-26 23:53:38 +0800

    Third Commit - City Tours 2

--- src/components/TourList/index.js
+++ src/components/TourList/index.js
@@ -8,6 +8,14 @@ export default class TourList extends Component {
     state = {
         tours: tourData
     }
+
+    removeTour = (id) => {
+        console.log(id)
+        const {tours} = this.state
+        const sortedTours = tours.filter(tour => tour.id !== id)
+        this.setState({tours: sortedTours})
+    }
+
     render() {
         const {tours} = this.state
         console.log(this.state.tours)
@@ -16,7 +24,7 @@ export default class TourList extends Component {
             <section className="tourlist">
                 {tours.map(tour => {
                     return(
-                        <Tour key={tour.id} tour={tour}/>
+                        <Tour key={tour.id} tour={tour} removeTour={this.removeTour}/>
                     )
                 })}
             </section>
